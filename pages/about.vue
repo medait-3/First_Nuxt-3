574fedde91c9a3f882ca49eb0b86f2bbc1912c35
meait-3 <tchikh3@@gmail.com>
2023-03-08 15:52:36 +0100

    add page

---
+++ pages/about.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+
+    </div>
+    <p>about</p>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+
+    </div>
+    <p>home</p>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+
+</style>
